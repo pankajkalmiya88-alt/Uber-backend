8f92e34861350c6b0ab6f884f3143cc418ab807d
pankajkalmiya88-alt <pankaj.k@supersourcing.com>
2026-02-18 11:53:58 +0530

    1. Complite basic authentication 2. register , login, get profile and logout

--- middlewares/auth.middleare.js
+++ middlewares/auth.middleare.js
@@ -4,18 +4,24 @@ const jwt = require('jsonwebtoken');
 
 
 module.exports.authUser = async (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
-    if(!token){
-        return res.status(401).json({message: "Unauthorized"});
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
     };
 
-    try{
+    const isBlacklisted = await userModel.findOne({ token: token });
+
+    if (isBlacklisted) {
+        return res.status(401).json({ message: "Unauthorized" }); 
+    }
+
+    try {
         const decode = jwt.verify(token, process.env.JWT_SECRET);
         const user = await userModel.findById(decode._id);
 
         req.user = user;
         return next();
-    }catch(err){
-        return res.status(401).json({message: "Unauthorized"});
+    } catch (err) {
+        return res.status(401).json({ message: "Unauthorized" });
     }
 }
